869dcefd5796f4acbf15802887c503d8223d179b
ayuguchi <ayuguchi@github.com>
2022-01-19 18:50:46 +0900

    Tested plotting AUs from an dumpfile

--- script/plot_AU.py
+++ script/plot_AU.py
@@ -15,25 +15,30 @@ au45 = []
 def main():
     f = open('../dumpfiles/1712F2006.csv')
     data = csv.reader(f)
-#    data = next(original_data)
+    label_data = next(data)
 #    print(type(data))
-    print(data)
-#    for row in data:
-#        for float
+    for row in data:
+        print(row)
 #        start = float(row[1])
-#        start_time.append(float(row[1]))
-#        au01.append(float(row[1420]))
-#        au02.append(float(row[1421]))
-#        au04.append(float(row[1422]))
-#        au05.append(float(row[1423]))
-#        au45.append(float(row[1437]))
+        start_time.append(float(row[1]))
+        au01.append(float(row[1420]))
+        au02.append(float(row[1421]))
+        au04.append(float(row[1422]))
+        au05.append(float(row[1423]))
+        au45.append(float(row[1437]))
     
     plt.xlabel('time')
     plt.ylabel('Action Units')
 
-#    plt.plot(start_time, au45, label=AU45)
+    plt.plot(start_time, au01, label='AU01')
+    plt.plot(start_time, au02, label='AU02')
+    plt.plot(start_time, au04, label='AU04')
+    plt.plot(start_time, au05, label='AU05')
+    plt.plot(start_time, au45, label='AU45')
     plt.grid()
     plt.show()
+
+    f.close()
 
 if __name__ == '__main__':
     main()
