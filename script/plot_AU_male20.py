ef7aacaa7cd27b1bd2b5fd9bfab0742ea58cc6d8
ayuguchi <ayuguchi@github.com>
2022-01-20 19:12:05 +0900

    Plots AUs from dumpfiles

--- script/plot_AU_male20.py
+++ script/plot_AU_male20.py
@@ -17,19 +17,6 @@ row = 2
 col = 3
 
 def main():
-    f_20f1 = open('../dumpfiles/1712F2006.csv')
-    d_f_20f1 = csv.reader(f_20f1)
-    label_d_f_20f1 = next(d_f_20f1)
-    f_20f2 = open('../dumpfiles/1712F2010.csv')
-    d_f_20f2 = csv.reader(f_20f2)
-    label_d_f_20f2 = next(d_f_20f2)
-    f_20f3 = open('../dumpfiles/1712F2018.csv')
-    d_f_20f3 = csv.reader(f_20f3)
-    label_d_f_20f3 = next(d_f_20f3)
-    f_20f4 = open('../dumpfiles/1712F2019.csv')
-    d_f_20f4 = csv.reader(f_20f4)
-    label_d_f_20f4 = next(d_f_20f4)
-
     f_20m1 = open('../dumpfiles/1712M2007.csv')
     d_f_20m1 = csv.reader(f_20m1)
     label_d_f_20m1 = next(d_f_20m1)
@@ -46,125 +33,6 @@ def main():
     d_f_20m5 = csv.reader(f_20m5)
     label_d_f_20m5 = next(d_f_20m5)
 
-    for d_row in d_f_20f1:
-        start_time.append(float(d_row[1]))
-        au01.append(float(d_row[1420]))
-        au02.append(float(d_row[1421]))
-        au04.append(float(d_row[1422]))
-        au05.append(float(d_row[1423]))
-        au45.append(float(d_row[1437]))
-
-    plt.subplot(col, row, 1)
-    plt.xlabel('time')
-    plt.ylabel('Action Units')
-    plt.plot(start_time, au01, label='AU01', marker='.')
-    plt.plot(start_time, au02, label='AU02', marker='.')
-    plt.plot(start_time, au04, label='AU04', marker='.')
-    plt.plot(start_time, au05, label='AU05', marker='.')
-    plt.plot(start_time, au45, label='AU45', marker='.')
-    plt.title('1712F2006')
-    plt.ylim(0.0,1.0)
-    plt.grid()
-    plt.legend()
-#    plt.show()
-
-    start_time.clear()
-    au01.clear()
-    au02.clear()
-    au04.clear()
-    au05.clear()
-    au45.clear()
-
-
-    for d_row in d_f_20f2:
-        start_time.append(float(d_row[1]))
-        au01.append(float(d_row[1420]))
-        au02.append(float(d_row[1421]))
-        au04.append(float(d_row[1422]))
-        au05.append(float(d_row[1423]))
-        au45.append(float(d_row[1437]))
-    
-    plt.subplot(col, row, 2)
-    plt.xlabel('time')
-    plt.ylabel('Action Units')
-    plt.plot(start_time, au01, label='AU01', marker='.')
-    plt.plot(start_time, au02, label='AU02', marker='.')
-    plt.plot(start_time, au04, label='AU04', marker='.')
-    plt.plot(start_time, au05, label='AU05', marker='.')
-    plt.plot(start_time, au45, label='AU45', marker='.')
-    plt.title('1712F2010')
-    plt.ylim(0.0,1.0)
-    plt.grid()
-    plt.legend()
-
-    start_time.clear()
-    au01.clear()
-    au02.clear()
-    au04.clear()
-    au05.clear()
-    au45.clear()
-
-
-    for d_row in d_f_20f3:
-        start_time.append(float(d_row[1]))
-        au01.append(float(d_row[1420]))
-        au02.append(float(d_row[1421]))
-        au04.append(float(d_row[1422]))
-        au05.append(float(d_row[1423]))
-        au45.append(float(d_row[1437]))
-    
-    plt.subplot(col, row, 3)
-    plt.xlabel('time')
-    plt.ylabel('Action Units')
-    plt.plot(start_time, au01, label='AU01', marker='.')
-    plt.plot(start_time, au02, label='AU02', marker='.')
-    plt.plot(start_time, au04, label='AU04', marker='.')
-    plt.plot(start_time, au05, label='AU05', marker='.')
-    plt.plot(start_time, au45, label='AU45', marker='.')
-    plt.title('1712F2018')
-    plt.ylim(0.0,1.0)
-    plt.grid()
-    plt.legend()
-
-    start_time.clear()
-    au01.clear()
-    au02.clear()
-    au04.clear()
-    au05.clear()
-    au45.clear()
-
-
-    for d_row in d_f_20f4:
-        start_time.append(float(d_row[1]))
-        au01.append(float(d_row[1420]))
-        au02.append(float(d_row[1421]))
-        au04.append(float(d_row[1422]))
-        au05.append(float(d_row[1423]))
-        au45.append(float(d_row[1437]))
-    
-    plt.subplot(col, row, 4)
-    plt.xlabel('time')
-    plt.ylabel('Action Units')
-    plt.plot(start_time, au01, label='AU01', marker='.')
-    plt.plot(start_time, au02, label='AU02', marker='.')
-    plt.plot(start_time, au04, label='AU04', marker='.')
-    plt.plot(start_time, au05, label='AU05', marker='.')
-    plt.plot(start_time, au45, label='AU45', marker='.')
-    plt.title('1712F2019')
-    plt.ylim(0.0,1.0)
-    plt.grid()
-    plt.legend()
-
-    start_time.clear()
-    au01.clear()
-    au02.clear()
-    au04.clear()
-    au05.clear()
-    au45.clear()
-
-    plt.tight_layout()
-    plt.show()
-'''
     for d_row in d_f_20m1:
         start_time.append(float(d_row[1]))
         au01.append(float(d_row[1420]))
@@ -173,7 +41,7 @@ def main():
         au05.append(float(d_row[1423]))
         au45.append(float(d_row[1437]))
     
-    plt.subplot(col, row, 5)
+    plt.subplot(col, row, 1)
     plt.xlabel('time')
     plt.ylabel('Action Units')
     plt.plot(start_time, au01, label='AU01', marker='.')
@@ -201,7 +69,7 @@ def main():
         au05.append(float(d_row[1423]))
         au45.append(float(d_row[1437]))
     
-    plt.subplot(col, row, 6)
+    plt.subplot(col, row, 2)
     plt.xlabel('time')
     plt.ylabel('Action Units')
     plt.plot(start_time, au01, label='AU01', marker='.')
@@ -229,7 +97,7 @@ def main():
         au05.append(float(d_row[1423]))
         au45.append(float(d_row[1437]))
     
-    plt.subplot(col, row, 7)
+    plt.subplot(col, row, 3)
     plt.xlabel('time')
     plt.ylabel('Action Units')
     plt.plot(start_time, au01, label='AU01', marker='.')
@@ -257,7 +125,7 @@ def main():
         au05.append(float(d_row[1423]))
         au45.append(float(d_row[1437]))
     
-    plt.subplot(col, row, 8)
+    plt.subplot(col, row, 4)
     plt.xlabel('time')
     plt.ylabel('Action Units')
     plt.plot(start_time, au01, label='AU01', marker='.')
@@ -285,7 +153,7 @@ def main():
         au05.append(float(d_row[1423]))
         au45.append(float(d_row[1437]))
     
-    plt.subplot(col, row, 9)
+    plt.subplot(col, row, 5)
     plt.xlabel('time')
     plt.ylabel('Action Units')
     plt.plot(start_time, au01, label='AU01', marker='.')
@@ -304,7 +172,9 @@ def main():
     au04.clear()
     au05.clear()
     au45.clear()
-'''
+
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == '__main__':
     main()
